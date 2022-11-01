c1f2d115d8d6958e84ad3a2d48e2f9c953ac5b75
bimo-a-f <bimofadli39@gmail.com>
2022-11-01 10:34:14 +0700

    Custom Favicon

--- resources/views/front/layout/head.blade.php
+++ resources/views/front/layout/head.blade.php
@@ -12,7 +12,7 @@
     <link
         href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Poppins:300,400,500,600,700|PT+Serif:400,400i&display=swap"
         rel="stylesheet" type="text/css" />
-    <link rel="icon" type="image/x-icon" href="{{asset('Backend/logo_12.png')}}" />
+    <link rel="icon" type="image/x-icon" href="{{env('CUSTOM_FAVICON','/Backend/logo_12.png')}}" />
     <link rel="stylesheet" href="{{asset('front/css/bootstrap.css')}}" type="text/css" />
     <link rel="stylesheet" href="{{asset('front/style.css')}}" type="text/css" />
     <link rel="stylesheet" href="{{asset('front/css/dark.css')}}" type="text/css" />
