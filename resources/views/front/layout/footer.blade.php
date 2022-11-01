356774f748146bbb1c0868c353ba0770b7e7184e
bimo-a-f <bimofadli39@gmail.com>
2022-11-01 13:38:25 +0700

    Url Encode slug

--- resources/views/front/layout/footer.blade.php
+++ resources/views/front/layout/footer.blade.php
@@ -70,7 +70,7 @@
                                 <div class="grid-inner row">
                                     <div class="col">
                                         <div class="entry-title">
-                                            <h4><a href="class/{{ $p->unique_id }}/{{ $p->title }}">{{ $p->title }}</a>
+                                            <h4><a href="class/{{ $p->unique_id }}/{{ urlencode($p->title) }}">{{ $p->title }}</a>
                                             </h4>
                                         </div>
                                         <div class="entry-meta">
