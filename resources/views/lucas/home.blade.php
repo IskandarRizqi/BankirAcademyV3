87539ed6c90a8fd38ac4cf71f996d362e0097999
bimo-a-f <bimofadli39@gmail.com>
2022-11-01 09:17:18 +0700

    homel

--- resources/views/lucas/home.blade.php
+++ resources/views/lucas/home.blade.php
@@ -174,7 +174,7 @@
         <div class="section border-top-0 mb-6">
             <div class="container text-center">
                 <div class="heading-block center">
-                    <h2>SEMUA KELAS</h2>
+                    <h2 id="kelassectiontitle">SEMUA KELAS</h2>
                 </div>
                 {{-- <div class="row" id="sld"> --}}
                     <div class="row" id="">
