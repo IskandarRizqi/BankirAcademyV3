7426fbad5d4cc0ede173840f624d192ef602cd76
dannykoes <dannykoes44@gmail.com>
2022-11-18 13:22:29 +0700

    persen di pricing class

--- resources/views/backend/classes/classpricingmodal.blade.php
+++ resources/views/backend/classes/classpricingmodal.blade.php
@@ -1,15 +1,23 @@
-<div class="modal fade modalwithselect2" id="classPricingModal" role="dialog" aria-labelledby="classPricingModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="classPricingModalLabel">Pricing</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="closemodal('#classPricingModal')">
-					<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
-                </button>
-            </div>
-            <div class="modal-body">
+<div class="modal fade modalwithselect2" id="classPricingModal" role="dialog" aria-labelledby="classPricingModalLabel"
+	aria-hidden="true">
+	<div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="classPricingModalLabel">Pricing</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"
+					onclick="closemodal('#classPricingModal')">
+					<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+						viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+						stroke-linejoin="round" class="feather feather-x">
+						<line x1="18" y1="6" x2="6" y2="18"></line>
+						<line x1="6" y1="6" x2="18" y2="18"></line>
+					</svg>
+				</button>
+			</div>
+			<div class="modal-body">
 				<h5 class="activeClassTitle">CLASS NAME GOES HERE</h5>
-				<form action="/admin/classes/setpricing" id="newClassesForm" method="POST" enctype="multipart/form-data">
+				<form action="/admin/classes/setpricing" id="newClassesForm" method="POST"
+					enctype="multipart/form-data">
 					@csrf
 					<input type="hidden" name="_method" value="POST" id="hdnClassesMethod">
 					<input type="hidden" name="hdnClassesId" value="0" class="hdnClassesId">
@@ -19,7 +27,8 @@
 							<div class="form-group">
 								<label for="numClassPrice">Price</label>
 								<small id="nomClassPrice">Rp.0,00</small>
-								<input type="number" name="numClassPrice" id="numClassPrice" class="form-control clsNumberOnPrice" value="0" required>
+								<input type="number" name="numClassPrice" id="numClassPrice"
+									class="form-control clsNumberOnPrice" value="0" required>
 							</div>
 							<hr>
 						</div>
@@ -32,14 +41,18 @@
 						<div class="col-lg-10">
 							<div class="form-group" hidden>
 								<label for="datPromoDateStart">Promo Date</label>
-								<small class="inputerrormessage text-danger" input-target="datPromoDateStart" style="display: none;"></small>
-								<small class="inputerrormessage text-danger" input-target="datPromoDateEnd" style="display: none;"></small>
+								<small class="inputerrormessage text-danger" input-target="datPromoDateStart"
+									style="display: none;"></small>
+								<small class="inputerrormessage text-danger" input-target="datPromoDateEnd"
+									style="display: none;"></small>
 								<div class="input-group mb-4">
-									<input type="date" class="form-control" name="datPromoDateStart" id="datPromoDateStart" placeholder="Promo Start" aria-label="Promo Start">
+									<input type="date" class="form-control" name="datPromoDateStart"
+										id="datPromoDateStart" placeholder="Promo Start" aria-label="Promo Start">
 									<div class="input-group-append">
 										<span class="input-group-text" id="basic-addon5">s/d</span>
 									</div>
-									<input type="date" class="form-control" name="datPromoDateEnd" id="datPromoDateEnd" placeholder="Promo End" aria-label="Promo End">
+									<input type="date" class="form-control" name="datPromoDateEnd" id="datPromoDateEnd"
+										placeholder="Promo End" aria-label="Promo End">
 								</div>
 							</div>
 						</div>
@@ -49,11 +62,13 @@
 								<small id="nomClassPromo">Rp.0,00</small>
 								<div class="row">
 									<div class="col-lg-9">
-										<input type="number" name="numClassPromo" id="numClassPromo" class="form-control clsNumberOnPrice" value="0">
+										<input type="number" name="numClassPromo" step="any" id="numClassPromo"
+											class="form-control clsNumberOnPrice" value="0">
 									</div>
 									<div class="col-lg-3">
 										<div class="input-group mb-4">
-											<input type="number" name="numClassPromoPrctg" id="numClassPromoPrctg" class="form-control clsNumberOnPrice" value="0">
+											<input type="number" name="numClassPromoPrctg" step="any"
+												id="numClassPromoPrctg" class="form-control clsNumberOnPrice" value="0">
 											<div class="input-group-append">
 												<span class="input-group-text">%</span>
 											</div>
@@ -63,11 +78,12 @@
 							</div>
 						</div>
 					</div>
-					<button class="btn" data-dismiss="modal" onclick="closemodal('#classPricingModal')"><i class="flaticon-cancel-12"></i> Discard</button>
+					<button class="btn" data-dismiss="modal" onclick="closemodal('#classPricingModal')"><i
+							class="flaticon-cancel-12"></i> Discard</button>
 					<button type="submit" class="btn btn-primary">Save</button>
 				</form>
-            </div>
-            <div class="modal-footer"></div>
-        </div>
-    </div>
+			</div>
+			<div class="modal-footer"></div>
+		</div>
+	</div>
 </div>
