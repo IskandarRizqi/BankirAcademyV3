f4c0cbc63953f06cf2303ccb6d37218801f8486c
dannykoes <dannykoes44@gmail.com>
2022-12-05 15:29:24 +0700

    class participant

--- app/Models/ClassesModel.php
+++ app/Models/ClassesModel.php
@@ -78,13 +78,13 @@ class ClassesModel extends Model
 		if (array_key_exists('id', $this->attributes)) {
 			$data['all'] = DB::table('class_payment')
 				->select('class_payment.*', 'user_profile.name', 'user_profile.phone_region', 'user_profile.phone', 'user_profile.gender', 'user_profile.instansi')
-				->join('user_profile', 'user_profile.user_id', 'class_payment.user_id')
+				->leftJoin('user_profile', 'user_profile.user_id', 'class_payment.user_id')
 				// ->where('class_payment.status', 1)
 				->where('class_payment.class_id', $this->attributes['id'])
 				->get();
 			$data['lunas'] = DB::table('class_payment')
 				->select('class_payment.*', 'user_profile.name', 'user_profile.phone_region', 'user_profile.phone', 'user_profile.gender', 'user_profile.instansi')
-				->join('user_profile', 'user_profile.user_id', 'class_payment.user_id')
+				->leftJoin('user_profile', 'user_profile.user_id', 'class_payment.user_id')
 				->where('class_payment.status', 1)
 				// ->where('class_payment.status', 0)
 				->where('class_payment.class_id', $this->attributes['id'])
