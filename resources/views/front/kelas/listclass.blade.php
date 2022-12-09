9545797c728ef2ac6c4d86dc5c98928224b66892
dannykoes <sandal2610@gmail.com>
2022-12-09 18:32:29 +0700

    row di listclass

--- resources/views/front/kelas/listclass.blade.php
+++ resources/views/front/kelas/listclass.blade.php
@@ -113,79 +113,82 @@
                                                     <label for="tipe-{{ $key }}"
                                                         class="checkbox-style-1-label">{{ $ctg }}</label>
                                                 </div>
+                                            </div>
                                         @endforeach
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <button type="submit" class="btn btn-primary btn-block btn-sm mt-2">Cari</button>
                 </div>
-                <button type="submit" class="btn btn-primary btn-block btn-sm mt-2">Cari</button>
-        </div>
 
-        </form>
-        <div class="row gutter-40 col-mb-80">
-            <div class="postcontent col-lg-9">
-                @if ($class['data'])
-                    <div class="single-event">
-                        <div class="row">
-                            @foreach ($class['data'] as $v)
-                                <div class="col-lg-4 col-sm-6 mb-4">
-                                    <div class="card">
-                                        <div class="card-body">
-                                            <div class="card" style="min-height: 0px !important">
-                                                <img src="{{ $v['image'] }}" width="100%">
-                                            </div>
-                                            <p class="mt-2 text-overflow" style="line-height: 1 !important;">
-                                                {{ substr($v['title'], 0, 115) }}... </p>
-                                            <h6 style="margin: 0px !important; font-weight: normal;">
-                                                @if ($v['date_start'] == $v['date_end'])
-                                                    {{ \Carbon\Carbon::parse($v['date_start'])->format('d-m-Y') }}
-                                                @else
-                                                    {{ \Carbon\Carbon::parse($v['date_start'])->format('d-m-Y') }} s/d
-                                                    {{ \Carbon\Carbon::parse($v['date_end'])->format('d-m-Y') }}
-                                                @endif
-                                            </h6>
-                                            <a href="/profile-instructor/3/Dani" class="d-flex mt-2">
-                                                <img class="mr-3 rounded-circle"
-                                                    src="/Image/{{ json_decode($v['instructor_list'][0]->picture)->url }}"
-                                                    alt="Generic" placeholder="" image=""
-                                                    style="max-width:50px; max-height:50px;">
-                                                <div class="">
-                                                    <label class="d-block mb-0">{{ $v['instructor_list'][0]->name }}
-                                                        {{-- <small>{{$v['instructor_list'][0]->title}}</small> --}}
-                                                    </label>
+            </form>
+            <div class="row gutter-40 col-mb-80">
+                <div class="postcontent col-lg-9">
+                    @if ($class['data'])
+                        <div class="single-event">
+                            <div class="row">
+                                @foreach ($class['data'] as $v)
+                                    <div class="col-lg-4 col-sm-6 mb-4">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <div class="card" style="min-height: 0px !important">
+                                                    <img src="{{ $v['image'] }}" width="100%">
+                                                </div>
+                                                <p class="mt-2 text-overflow" style="line-height: 1 !important;">
+                                                    {{ substr($v['title'], 0, 115) }}... </p>
+                                                <h6 style="margin: 0px !important; font-weight: normal;">
+                                                    @if ($v['date_start'] == $v['date_end'])
+                                                        {{ \Carbon\Carbon::parse($v['date_start'])->format('d-m-Y') }}
+                                                    @else
+                                                        {{ \Carbon\Carbon::parse($v['date_start'])->format('d-m-Y') }}
+                                                        s/d
+                                                        {{ \Carbon\Carbon::parse($v['date_end'])->format('d-m-Y') }}
+                                                    @endif
+                                                </h6>
+                                                <a href="/profile-instructor/3/Dani" class="d-flex mt-2">
+                                                    <img class="mr-3 rounded-circle"
+                                                        src="/Image/{{ json_decode($v['instructor_list'][0]->picture)->url }}"
+                                                        alt="Generic" placeholder="" image=""
+                                                        style="max-width:50px; max-height:50px;">
+                                                    <div class="">
+                                                        <label
+                                                            class="d-block mb-0">{{ $v['instructor_list'][0]->name }}
+                                                            {{-- <small>{{$v['instructor_list'][0]->title}}</small> --}}
+                                                        </label>
+                                                        @if ($v['pricing'])
+                                                            @if ($v['pricing']->promo)
+                                                                <span class="d-block mb-0"> Harga</span>
+                                                                <del>Rp.
+                                                                    {{ number_format($v['pricing']->price) }}</del>
+                                                            @else
+                                                                <span class="d-block mb-0"> Harga</span>
+                                                                <small>{{ $v['pricing']->price }}</small>
+                                                            @endif
+                                                        @endif
+                                                    </div>
+                                                    {{-- <div class="ml-2 flex-fill text-right">
+                                            </div> --}}
+                                                </a>
+                                                <div class="text-center mt-2 w-100">
                                                     @if ($v['pricing'])
                                                         @if ($v['pricing']->promo)
-                                                            <span class="d-block mb-0"> Harga</span>
-                                                            <del>Rp. {{ number_format($v['pricing']->price) }}</del>
-                                                        @else
-                                                            <span class="d-block mb-0"> Harga</span>
-                                                            <small>{{ $v['pricing']->price }}</small>
                                                         @endif
+                                                        <h3 style=" color:#139700 !important;">Rp.
+                                                            {{ number_format($v['pricing']->price - $v['pricing']->promo_price) }}<span
+                                                                class="badge badge-danger badge-sm ml-2">{{ ($v['pricing']->promo_price / $v['pricing']->price) * 100 }}
+                                                                %</span></h3>
                                                     @endif
+                                                    <a class="btn btn-primary btn-block btn-rounded"
+                                                        style="border-radius:10px !important"
+                                                        href="/class/{{ $v['unique_id'] }}/{{ str_replace('/', '-', $v['title']) }}">
+                                                        Detail </a>
                                                 </div>
-                                                {{-- <div class="ml-2 flex-fill text-right">
-                                            </div> --}}
-                                            </a>
-                                            <div class="text-center mt-2 w-100">
-                                                @if ($v['pricing'])
-                                                    @if ($v['pricing']->promo)
-                                                    @endif
-                                                    <h3 style=" color:#139700 !important;">Rp.
-                                                        {{ number_format($v['pricing']->price - $v['pricing']->promo_price) }}<span
-                                                            class="badge badge-danger badge-sm ml-2">{{ ($v['pricing']->promo_price / $v['pricing']->price) * 100 }}
-                                                            %</span></h3>
-                                                @endif
-                                                <a class="btn btn-primary btn-block btn-rounded"
-                                                    style="border-radius:10px !important"
-                                                    href="/class/{{ $v['unique_id'] }}/{{ str_replace('/', '-', $v['title']) }}">
-                                                    Detail </a>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                                {{-- <a href="/class/{{ $v['unique_id'] }}/{{ urlencode($v['title']) }}">
+                                    {{-- <a href="/class/{{ $v['unique_id'] }}/{{ urlencode($v['title']) }}">
                                 <div class="card">
                                     <div class="card-body">
                                         <img src="" alt="Thumbnail" style="width: 130px;max-height:75px;">
@@ -196,32 +199,32 @@
                                     </div>
                                 </div>
                             </a> --}}
-                            @endforeach
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-lg-12 text-center">
-                                <nav aria-label="Page navigation blog">
-                                    <ul class="pagination justify-content-center">
-                                        @foreach ($class['links'] as $k => $p)
-                                            <li class="page-item {{ $p['active'] ? 'active' : '' }}"><a
-                                                    class="page-link" href="{{ $p['url'] }}">
-                                                    <?= $p['label'] ?>
-                                                </a></li>
-                                        @endforeach
-                                    </ul>
-                                </nav>
+                                @endforeach
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="col-lg-12 text-center">
+                                    <nav aria-label="Page navigation blog">
+                                        <ul class="pagination justify-content-center">
+                                            @foreach ($class['links'] as $k => $p)
+                                                <li class="page-item {{ $p['active'] ? 'active' : '' }}"><a
+                                                        class="page-link" href="{{ $p['url'] }}">
+                                                        <?= $p['label'] ?>
+                                                    </a></li>
+                                            @endforeach
+                                        </ul>
+                                    </nav>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                @endif
+                    @endif
+                </div>
+                <div class="col-lg-3">
+                    {{-- --}}
+                </div>
             </div>
-            <div class="col-lg-3">
-                {{-- --}}
-            </div>
-        </div>
 
-    </div>
+        </div>
     </div>
 </section><!-- #content end -->
 @section('custom-js')
